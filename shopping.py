73d33017d1159b498897d96e01c98e59c1067104
Joe.rackzy <josephameh686@gmail.com>
2026-07-17 15:06:13 +0100

    shopping.py

---
+++ shopping.py
@@ -0,0 +1,41 @@
+for b in range(9):
+    if b == 7:
+        break
+    print(f"processing my count.....{b}")
+print("")
+for c in range(5):
+    if c == 4:
+        continue
+    print(f"start countinig for here{c}")
+
+for i in range(6):
+    if i == 2:
+        continue
+    print(f"Token: {i}")
+
+
+count = 10
+while count > 3:
+    print(f"we are moving by the lord: {count}") 
+
+    count = count - 1
+print("we are done")    
+
+for i in range(8):
+    if i == 4:
+        continue
+    print(f"i am unmber: {i}")
+
+for b in range(8):
+   if b == 7:
+       break 
+   print(f"we can now stop here: {b}")
+
+students = ["joshua", "mary", "jeams", "pious"]
+for item in students:
+       print(f"this is: {item}")
+
+for c in range(12):
+    if c == 7:  
+        break
+    print(f"processing my counting... {c}")  

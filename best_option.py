d27341dd4af6c8d82681e728f26d887d1529d5ba
joe.rackzy <josephameh686@gmail.com.com>
2026-09-03 17:19:23 +0100

    best_option

---
+++ best_option.py
@@ -0,0 +1,12 @@
+class readability:
+    def best_option(self):
+        return "C"
+    
+    def test_readability():
+        return reader.best_option()
+    
+    def solution():
+        return reader.best_option()
+reader = readability()   
+best_option = "C"
+print(reader.best_option())   

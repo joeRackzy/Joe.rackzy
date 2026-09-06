a7dc3e85102ad4855f597c901bb605a942defdda
joerackzy <josephameh686@gmail.com>
2026-09-06 15:43:57 +0100

    tuple_list

--- cude.py
+++ cude.py
@@ -1,7 +1,74 @@
-print("thi is the a example of cube ""\n".upper())
-cube = []
-for value in range(1,31):
-    cube_unmber = value**3
-    cube.append(cube_unmber)
-    print(cube_unmber)
-print(cube)   
+# # print("thi is the a example of cube ""\n".upper())
+# # cube = []
+# # for value in range(1,31):
+# #     cube_unmber = value**3
+# #     cube.append(cube_unmber)
+# #     print(cube_unmber)
+# # print(cube)   
+
+
+
+
+
+
+# man = list(range(2,11,2))
+# print(man[2:])
+
+# for p in sorted(range(2,11,2)):
+#     print(p)
+# print("")    
+
+# Cubes = []
+# for value in range(1,10):
+#     Cube = value**3
+#     Cubes.append(Cube)
+#     print(Cube)    
+# print(man[-2:])    
+
+# players = ["palmer", "sanches", "Gustone", "mane", "raphina", "Dembele"]
+# print("tis are the first three players in the team")
+# for player in players[2:4]:
+#     print(player)
+
+
+
+# slice = ["palmer", "sanches", "Gustone", "mane", "raphina", "Dembele","colobanycv "]
+# print("this is the first three items in my list.")
+# print(slice[0:3])
+# 
+# print("this are the three items frim the middle.")
+# print(slice[2:5])
+# print("this are the last three items in the list")
+# print(slice[-3:])
+# 
+# 
+# 
+# 
+# 
+# # 
+# my_pizzas = ["peperino pizza", "teasty pizza", "ginger pizza", "flat size"]
+# my_frind_pizzas = my_pizzas[:]
+# my_pizzas.append("great size")
+# my_frind_pizzas.insert(0, "sailtino pizza")
+# print("My favorite pizza are ..")
+
+# for my_pizza in my_pizzas:
+#     print(my_pizza)
+
+# print("My friend's favorite are")
+# for My_friend_pizza in my_frind_pizzas:
+#     print(My_friend_pizza)
+
+
+print("buffet restaurant menu list" "\n".upper())
+print("this is our old menu" "\n")
+
+buffet = ("fish and chips", "coffe and tea", "rice and beans", "peppe maets", "fruit sallard")
+for food in buffet:
+    print(food)
+
+print("\nthis is the rewrite menu " "\n")    
+buffet = ("fish and chips", "coffe and tea", "rice and beans", "pizza", "toasted bread")
+for rewrite in buffet:
+    print(rewrite)           
+
---
+++ tuple_llist.py
@@ -0,0 +1,11 @@
+print("buffet restaurant menu list" "\n".upper())
+print("this is our old menu" "\n")
+
+buffet = ("fish and chips", "coffe and tea", "rice and beans", "peppe maets", "fruit sallard")
+for food in buffet:
+    print(food)
+
+print("\nthis is the rewrite menu " "\n")    
+buffet = ("fish and chips", "coffe and tea", "rice and beans", "pizza", "toasted bread")
+for rewrite in buffet:
+    print(rewrite)

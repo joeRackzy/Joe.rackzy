e9248f8f42ef2a3376995df8e3630467427d70f1
joe.rackzy <josephameh686@gmail.com>
2026-09-07 16:32:23 +0100

    statement_if

---
+++ statment_if.py
@@ -0,0 +1,35 @@
+for bady in range(1,3):
+    print(f"we are moving forward {bady}")
+    if bady == 1:
+        print("section 1" )
+
+
+
+
+
+age = 19
+
+if age >= 18:
+
+    print("\nyou are old enough to vote in your country!")
+    print("have you registerd for voters card?")
+    registerd_list = ["joseph", "john","mary","mike", "faith", "john"]
+
+    if registerd_list != "joseph":
+        print("yes i have register for voters card")
+        
+    else:
+       
+        print("yet to be register" "\n" + "so you have to go for voting")
+    
+elif age == 18:
+    print("wait for next year when you are up to adult!")
+             
+else:
+    print("not yet an adult")
+
+
+    
+
+
+   

bfb158e1245257f28bc97e9d8133168a9c5ac69d
joe.rackzy <josephameh686@gmail.com>
2026-09-08 10:05:12 +0100

    if_testing.py

---
+++ if_testing.py
@@ -0,0 +1,10 @@
+registerd_list = ["red","Green", "yellow",]
+if "Green" in registerd_list:
+    print("5 point")
+if "yellow" in registerd_list:
+    print("10 point")
+if "regina" in registerd_list:
+    print("invalid")        
+if "red" in registerd_list:
+    print("15 point\n")    
+print("")    

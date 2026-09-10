5a8504ff639d4c322a08b31900596e0303354831
joe.rackzy <josephameh686@gmail.com>
2026-09-10 14:28:34 +0100

    claude_project

---
+++ claude_project.py
@@ -0,0 +1,22 @@
+claude_Guests = ["Alice", "Bob", "Charlie", "David", "Joseph"]
+for Guest in claude_Guests:
+    print(f"hi {Guest}: you are invited to a diiner in my house tonight.")
+claude_Guests.remove("Alice")  
+print("\nsomebody is not comming to the dinnerto nightso we remove him for the list")  
+claude_Guests.insert(0,"Jene")
+claude_Guests.append("faith")
+
+for Guest in claude_Guests:
+    
+    print((Guest))
+sorted_list = sorted(claude_Guests)
+print(f"the is the sorted list \n{sorted_list}")
+print(f"this is the original list \n{claude_Guests}")
+# while len(claude_Guests) > 0:
+#     removed_Guest = claude_Guests.pop()
+#     print(f"removed {removed_Guest} from the list")
+Guest_count = len(claude_Guests)
+print(f"the number of guests remaining is: {Guest_count}")
+
+   
+

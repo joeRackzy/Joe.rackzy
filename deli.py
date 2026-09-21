83ad58a072b23605928beab8f55466e9a042519e
joe.rackzy <josephameh.com>
2026-09-21 15:21:53 +0100

    deli.py

--- deli.py
+++ deli.py
@@ -1,5 +1,8 @@
-sandwish_orders =["show", "YBNL", "content creator", "canada", "LonDon"]
+sandwish_orders =["show", "YBNL", "pastrami","content creator", "pastrami", "canada", "LonDon", "pastrami"]
 finished_sandwiches = []
+print("\n\tdeli has run out of pastrami")
+while "pastrami" in sandwish_orders:
+    sandwish_orders.remove("pastrami")
 while sandwish_orders:
     procesing = sandwish_orders.pop()
     print(f"{procesing} is the place i would lov eto go somedays")

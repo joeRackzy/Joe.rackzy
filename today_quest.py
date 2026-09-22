78b9837ab20965140c63f22924cc900c0d82443a
joe.rackzy <josephameh.com>
2026-09-22 11:30:11 +0100

    today_quest.py git add .
    git comiit -m today_quest.py

---
+++ today_quest.py
@@ -0,0 +1,7 @@
+invited = ["john", "faith", "Gina", "winner", "peter" "faith",]
+confirmed = []
+while invited:
+  
+    confirmed = invited.pop()
+    print(f"you are invited {confirmed}")
+print(invited) 

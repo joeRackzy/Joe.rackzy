0ccfdd79026b10c19db8d1347db33ca28e383cbf
joe.rackzy <josephamhe686@gmali.com>
2026-09-24 14:01:16 +0100

    main

---
+++ main.py
@@ -0,0 +1,9 @@
+import my_function
+my_function.greet("mary")
+
+
+import my_function
+my_function.squre(10)
+
+import my_function
+my_function.add_number(50,6)
---
+++ my_function.py
@@ -0,0 +1,10 @@
+def greet(name):
+    print(f"Hello {name}!.")
+
+
+def squre(number):
+    print(number * number)
+
+def add_number(a,b):
+    print(a*b)      
+
